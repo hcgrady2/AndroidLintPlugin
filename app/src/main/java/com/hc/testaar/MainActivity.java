6d265705878eca2dd0915e2fcfee441c67b07ea5
wanghaichao <hcwang1024@163.com>
2021-05-19 14:44:49 +0800

    aar lint ✊没有问题

--- app/src/main/java/com/hc/testaar/MainActivity.java
+++ app/src/main/java/com/hc/testaar/MainActivity.java
@@ -3,6 +3,8 @@ package com.hc.testaar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("", "'");
+        Log.d("", "'");
+        System.out.print("ddd");
+
+        Message msg = new Message();
+
+
+
+
+
+        Log.d("", "'");
+        Log.d("", "'");
+        System.out.print("ddd");
+
+        Message msg2 = new Message();
+
+
+
+
+
+
     }
 }
