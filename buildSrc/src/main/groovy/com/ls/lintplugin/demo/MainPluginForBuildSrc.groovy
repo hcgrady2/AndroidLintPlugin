dccaeebb69fac883cf35ad709ed8e7de96afb8ee
wanghaichao <hcwang1024@163.com>
2021-05-20 14:39:08 +0800

    add hook

--- buildSrc/src/main/groovy/com/ls/lintplugin/demo/MainPluginForBuildSrc.groovy
+++ buildSrc/src/main/groovy/com/ls/lintplugin/demo/MainPluginForBuildSrc.groovy
@@ -18,5 +18,37 @@ public class MainPluginForBuildSrc implements Plugin<Project> {
             println project.pluginsrc.message
         }
 
+
+
+        /*
+ * gradle task: 将git hooks 脚本复制到.git/hooks文件夹下
+ * 根据不同的系统类型复制不同的git hooks脚本(现支持Windows、Linux两种)
+ */
+        project.task("testInstall").doLast {
+            println(" testInstall task 开始执行 ---->")
+
+//            println("OS Type:" + System.getProperty("os.name"))
+//            File postCommit
+//            String OSType = System.getProperty("os.name")
+//            if (OSType.contains("Windows")) {
+//                postCommit = new File(project.rootDir, "post-commit-windows")
+//            } else {
+//                println("============ mac 安装脚本  ===============")
+//
+//                postCommit = new File(project.rootDir, "post-commit")
+//            }
+
+//            project.copy {
+//                from (postCommit) {
+//                    rename {
+//                        String filename ->
+//                            "post-commit"
+//                    }
+//                }
+//                into new File(project.rootDir, ".git/hooks/")
+//            }
+        }
+
+
     }
 }
